67947dc3cf659cf3225a2ab9f9f4e68d3d3ca8bd
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-17 20:30:42 -0400

    Font Change
    Fuente de Geins a Open Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/layout/header/Header';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
+const OpenSans = Open_Sans({
+	weight: '400',
 	subsets: ['latin'],
 });
 
@@ -40,9 +35,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${OpenSans.className} antialiased`}>
 				<Header />
 				{children}
 			</body>
