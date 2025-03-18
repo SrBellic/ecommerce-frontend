e1bd41072c857c74f3456c098b1916591c839a9d
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-17 20:51:52 -0400

    Update Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { SearchBar } from '@/components/features/Input/SearchBar';
 import ShoppingKart from '@/components/features/Button/Shopping/ShoppingKart';
+import Card from '@/components/features/Product/Card/Card';
 
 export default function Home() {
 	return (
 		<div className='flex justify-around'>
 			<SearchBar />
 			<ShoppingKart />
+			<Card />
 		</div>
 	);
 }
