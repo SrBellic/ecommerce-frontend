85eac718e3aaad6184514d24a55c33cd71d8c76c
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-17 20:32:08 -0400

    Add (Shopping Kart)
    Shopping Kart template añadido

--- components/features/Button/Shopping/ShoppingKart.tsx
+++ components/features/Button/Shopping/ShoppingKart.tsx
@@ -9,18 +9,20 @@ export default function ShoppingKart() {
 
 	const incrementKart = () => setKart((prev) => prev + 1);
 	return (
-		<div className='flex justify-center items-center relative'>
-			<button
-				className='bg-yellow-500 text-white rounded-full px-2 py-1.5 hover:px-1.5 hover:py-1 cursor-pointer hover:bg-white hover:text-yellow-500 hover:border-2 hover:border-yellow-500'
-				onClick={incrementKart}
-			>
-				<ShoppingKartIcon
-					width={28}
-					height={28}
-					className='inline-block'
-				/>
-				{kart != 0 ? <span className='badge'>{kart}</span> : null}
-			</button>
-		</div>
+		<>
+			<div className='flex justify-center items-center relative'>
+				<button
+					className='transition bg-yellow-500 text-white rounded-full px-2 py-1.5 cursor-pointer hover:bg-white hover:text-yellow-500 hover:outline-2 hover:outline-yellow-500'
+					onClick={incrementKart}
+				>
+					<ShoppingKartIcon
+						width={28}
+						height={28}
+						className='inline-block'
+					/>
+					{kart != 0 ? <span className='badge'>{kart}</span> : null}
+				</button>
+			</div>
+		</>
 	);
 }
