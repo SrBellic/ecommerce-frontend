eb59ee72dfa60a718f439c9f4451bbd4efc9bc09
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-21 16:18:12 -0400

    Add (Interfaces)
    Directorio para interfaces. Añadida la interfaz para los productos

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,17 @@
+export interface Product {
+	id: number;
+	name: string;
+	created_by: number;
+	marca: string;
+}
+/*interface Product {
+	product: {
+		name: string;
+		description: string;
+		price: number;
+		stock: number;  //Interfaz que se usará
+		color_id: number;
+		category_id: string;
+		status: boolean;
+	};
+}*/
