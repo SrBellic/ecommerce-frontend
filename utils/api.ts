c2f1c5a74d526f562a17d6a64e0d977a266ce20a
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-21 15:33:19 -0400

    Fix Api Instance

--- utils/api.ts
+++ utils/api.ts
@@ -3,9 +3,11 @@ import axios from 'axios';
 // Crear una instancia de axios con la URL base de la API
 // y el tipo de contenido JSON
 
-export const api = axios.create({
+const api = axios.create({
 	baseURL: 'http://localhost:3001', // URL base de la API
 	headers: {
 		'Content-Type': 'application/json',
 	},
 });
+
+export default api;
