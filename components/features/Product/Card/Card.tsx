1cf49ee77e2cbda8c672bdfcbea5f198b46e2013
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-23 19:37:39 -0400

    Fix Card Styles

--- components/features/Product/Card/Card.tsx
+++ components/features/Product/Card/Card.tsx
@@ -4,33 +4,31 @@ import { useState, useEffect } from 'react';
 import fetchProduct from '../fetchProduct';
 import Image from 'next/image';
 import './Card.css';
+import Button from '../../Button/Button';
+import ShoppingKartIcon from '@/public/statics/SVG/shopping-cart.svg';
 import testImage from '@/public/statics/JPEG/TEST5.png';
 
 import { Product } from '@/interfaces/interfaces';
 
 export default function Card() {
-	const [showMessage, setShowMessage] = useState(false);
-	const [product, setProduct] = useState<Product | null>(null);
+	const [showButton, setShowButton] = useState(false);
+	// const [product, setProduct] = useState<Product | null>(null);
 
-	useEffect(() => {
-		async function fetchProductData() {
-			const data = await fetchProduct(1); // ID del producto que deseas obtener
-			setProduct(data); // Actualiza el estado con los datos obtenidos
-		}
+	// useEffect(() => {
+	// 	async function fetchProductData() {
+	// 		const data = await fetchProduct(1); // ID del producto que deseas obtener
+	// 		setProduct(data); // Actualiza el estado con los datos obtenidos
+	// 	}
 
-		fetchProductData();
-	}, []);
+	// 	fetchProductData();
+	// }, []);
 
 	return (
 		<>
 			<article
-				className='flex flex-col justify-center items-center outline-1 outline-zinc-200 rounded-md cursor-pointer'
-				onMouseEnter={() => {
-					setShowMessage(true);
-				}}
-				onMouseOut={() => {
-					setShowMessage(false);
-				}}
+				className='w-full sm:w-max px-3 pb-4 flex flex-col justify-center items-center rounded-md cursor-pointer hover:shadow-2xl relative'
+				onMouseEnter={() => setShowButton(true)}
+				onMouseLeave={() => setShowButton(false)}
 			>
 				<Image
 					src={testImage}
@@ -38,33 +36,26 @@ export default function Card() {
 					width={250}
 					height={250}
 					className='blur-card'
-					onMouseEnter={() => {
-						setShowMessage(true);
-					}}
 				/>
-				<div
-					className='flex flex-col ms-4 w-full rounded-b-md'
-					onMouseEnter={() => {
-						setShowMessage(true);
-					}}
-				>
+				<div className='flex flex-col w-full rounded-b-md px-5'>
 					<span className='text-zinc-400 py-2 text-xs'>
-						ID: {product?.id ?? 'Cargando...'}
+						ID: 12345 {/*product?.id ?? 'Cargando...'*/}
 					</span>
-					<h3>{product?.name ?? 'Cargando...'}</h3>
-					<span className='text-sm text-gray-500'>{product?.marca ?? ''}</span>
-					<span className='text-xl py-2'>10$</span>
+					<h3>Bandolero{/*product?.name ?? 'Cargando...'*/}</h3>
+					<span className='text-sm text-gray-500'>
+						Grenobil{/*product?.marca ?? ''*/}
+					</span>
+					<span className='text-xl py-2 text-right'>10$</span>
 				</div>
-				{showMessage ? (
-					<div
-						className='absolute outline-2 outline-white bg-yellow-400 rounded-full px-2 text-white font-bold'
-						onMouseEnter={() => {
-							setShowMessage(true);
-						}}
-					>
-						<p>Comprar</p>
-					</div>
-				) : null}
+				<div className={`invisible ${showButton ? 'visible' : null}`}>
+					<Button text='Agregar al carrito'>
+						<ShoppingKartIcon
+							width={28}
+							height={28}
+							className='inline-block'
+						/>
+					</Button>
+				</div>
 			</article>
 		</>
 	);
