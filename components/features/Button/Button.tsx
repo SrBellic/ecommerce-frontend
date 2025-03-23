912ba34fdb8f469333a5e50d71a2f6c96fafb835
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-03-23 19:36:11 -0400

    Feat (Button)
    Componente Botón funcionando

--- components/features/Button/Button.tsx
+++ components/features/Button/Button.tsx
@@ -1,13 +1,21 @@
 import { ReactNode } from 'react';
 
 export default function Button({
+	text,
 	children,
+	onPress,
 }: Readonly<{
+	text?: string;
 	children: React.ReactNode;
+	onPress?: () => void;
 }>) {
 	return (
 		<>
-			<button className='bg-yellow-500 text-white rounded-full px-2 py-1.5 cursor-pointer hover:bg-white hover:text-yellow-500 hover:outline-2 hover:outline-yellow-500'>
+			<button
+				onClick={onPress}
+				className='bg-yellow-500 text-white rounded-md px-2 py-1.5 cursor-pointer hover:bg-white hover:text-yellow-500 hover:outline-2 hover:outline-yellow-500 flex'
+			>
+				<span className='mx-2'>{text}</span>
 				<span>{children}</span>
 			</button>
 		</>
--- components/features/Button/Shopping/ShoppingKart.tsx
+++ components/features/Button/Shopping/ShoppingKart.tsx
@@ -4,17 +4,15 @@ import { useState } from 'react';
 import './ShoppingKart.css';
 import ShoppingKartIcon from '@/public/statics/SVG/shopping-cart.svg';
 
-export default function ShoppingKart() {
-	const [kart, setKart] = useState(0);
-
-	const incrementKart = () => setKart((prev) => prev + 1);
+export default function ShoppingKart({
+	kart,
+}: Readonly<{
+	kart: number;
+}>) {
 	return (
 		<>
 			<div className='flex justify-center items-center relative'>
-				<button
-					className='transition bg-yellow-500 text-white rounded-full px-2 py-1.5 cursor-pointer hover:bg-white hover:text-yellow-500 hover:outline-2 hover:outline-yellow-500'
-					onClick={incrementKart}
-				>
+				<button className='transition bg-yellow-500 text-white rounded-full px-2 py-1.5 cursor-pointer hover:bg-white hover:text-yellow-500 hover:outline-2 hover:outline-yellow-500'>
 					<ShoppingKartIcon
 						width={28}
 						height={28}
