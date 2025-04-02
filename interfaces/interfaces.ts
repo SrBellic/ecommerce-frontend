53c8a9b8af223d8afce83544e6531cc1db48e792
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-02 01:11:21 -0400

    Add User interface

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -4,6 +4,17 @@ export interface Product {
 	created_by: number;
 	marca: string;
 }
+
+export interface User {
+	id: number;
+	name: string;
+	lastName: string;
+	cedula: number | string;
+	email: string;
+	password: string;
+	phone: number | string;
+	address: string;
+}
 /*interface Product {
 	product: {
 		name: string;
