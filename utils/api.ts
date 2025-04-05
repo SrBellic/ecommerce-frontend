4362dd8fd2a9e0b212792736705916a5c406bd06
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-05 02:44:54 -0400

    URL Updated

--- utils/api.ts
+++ utils/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // y el tipo de contenido JSON
 
 const api = axios.create({
-	baseURL: 'http://localhost:3001', // URL base de la API
+	baseURL: 'http://localhost:3001/api/', // URL base de la API
 	headers: {
 		'Content-Type': 'application/json',
 	},
