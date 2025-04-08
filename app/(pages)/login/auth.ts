8129bb723c21b3bef65c883d5d133e3a26901f02
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-08 03:01:36 -0400

    Hotfix (Login)
    Eliminada condición para validar si los datos de un usuario existian o no. La condición arrojaba un error y no permitia continuar con la autenticación, por lo mismo no lograba realizarse el login

--- app/(pages)/login/auth.ts
+++ app/(pages)/login/auth.ts
@@ -14,11 +14,6 @@ async function Auth(formData: FormData): Promise<void> {
 			password: formData.get('password') as string,
 		};
 
-		//Validación temporal que valida que los datos se encuentren en la BBDD
-		if (!user.email || !user.password) {
-			throw new Error('Datos inválidos');
-		}
-
 		const response = await api.post(`users/login`, user);
 
 		if (response.status !== 200) {
