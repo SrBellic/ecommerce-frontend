741ab97992bcc2cdf5d1cf327791b5208502fa5a
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-29 18:56:55 -0400

    Refact (Get Product)
    Renderizado y Enrutado dinámico de los productos buscados por URL

--- app/(pages)/products/[productId]/page.tsx
+++ app/(pages)/products/[productId]/page.tsx
@@ -3,12 +3,18 @@ import fetchProduct from '@/components/features/Product/fetchProduct';
 export default async function Page({
 	params,
 }: {
-	params: { productId: string | number };
+	params: Promise<{ productId: string }>;
 }) {
-	const { productId } = params;
+	const { productId } = await params;
+
+	// Convertir productId a número y validar
+	const productIdNumber = Number(productId);
+	if (isNaN(productIdNumber)) {
+		return <div>ID de producto inválido</div>;
+	}
 
 	// Usar fetchProduct para obtener el producto
-	const product = await fetchProduct(Number(productId));
+	const product = await fetchProduct(productIdNumber);
 
 	if (!product) {
 		return <div>Producto no encontrado</div>;
@@ -17,8 +23,11 @@ export default async function Page({
 	return (
 		<div>
 			<h1>{product.name}</h1>
-			<p>{product.description}</p>
+			<p>Marca: {product.brand}</p>
+			<p>{!product.description ? 'N/A' : product.description}</p>
 			<p>Precio: ${product.price}</p>
+			<p>color: {product.color}</p>
+			<p>disponible: {product.stock}</p>
 		</div>
 	);
 }
