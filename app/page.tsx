1382a7d64a992673be57b8587246de7830f98aaf
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-29 19:14:49 -0400

    Update Index Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { SearchBar } from '@/components/features/Input/SearchBar';
-import ShoppingKart from '@/components/features/Button/Shopping/ShoppingKart';
+import ShoppingKart from '@/components/UI/Button/Shopping/ShoppingKart';
 import Link from 'next/link';
 import Carousel from '@/components/layout/content/Carousel';
 
