4c97f54624da7bc491077903b1e5821f76079644
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-04-30 22:33:50 -0400

    Feat (Metadata Generated)
    Se creó la generación dinámica de metadata de los productos unicos

--- app/(pages)/products/[productId]/page.tsx
+++ app/(pages)/products/[productId]/page.tsx
@@ -1,20 +1,58 @@
 import { fetchProduct } from '@/components/features/Product/fetchProduct';
+import { Product } from '@/interfaces/interfaces';
+import { Metadata } from 'next';
+import { Capitalize } from '@/utils/utils';
 
-export default async function Page({
-	params,
-}: {
+type ProductMetadata = Pick<
+	Product,
+	'id' | 'name' | 'description' | 'brand' | 'category'
+>;
+
+type Props = {
 	params: Promise<{ productId: string }>;
-}) {
+};
+
+//Functión para generar metadata dinámicamente
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const { productId } = await params;
+
+	const productIdNumber = Number(productId);
+
+	const product = await fetchProduct(productIdNumber);
+
+	if (isNaN(productIdNumber)) {
+		return {
+			title: 'Producto inválido',
+			description: 'El producto no es válido',
+		};
+	}
+
+	if (!product) {
+		return {
+			title: 'Producto no encontrado',
+			description: 'El producto no fue encontrado',
+		};
+	}
+	const { name, description, brand, category }: ProductMetadata = product;
+
+	return {
+		title: Capitalize(name),
+		description: Capitalize(description) || 'No hay descripción disponible',
+		keywords: [brand, name, category].map((word) => Capitalize(word)),
+	};
+}
+
+export default async function Page({ params }: Props) {
 	const { productId } = await params;
 
 	// Convertir productId a número y validar
 	const productIdNumber = Number(productId);
-	if (isNaN(productIdNumber)) {
-		return <div>ID de producto inválido</div>;
-	}
 
 	// Usar fetchProduct para obtener el producto
 	const product = await fetchProduct(productIdNumber);
+	if (isNaN(productIdNumber)) {
+		return <div>Producto inválido</div>;
+	}
 
 	if (!product) {
 		return <div>Producto no encontrado</div>;
