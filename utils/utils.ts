1e29fe1729f340f9074e968052e034f0d451f7e6
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-05-06 23:32:34 -0400

    Refact (Util Function)
    Se cambió la ubicación de la función que renderiza un tamaño máximo de caracteres en la descripción de los productos

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,12 @@
 export function Capitalize(string: string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 } // Capitaliza la primera letra de una cadena
+
+export function maxDescriptionLength(description: string, length: number) {
+	if (!description) {
+		return 'No hay descripción disponible.';
+	}
+	return description.length > length
+		? description.slice(0, length) + '...'
+		: description;
+} // Longitud máxima de la descripción
